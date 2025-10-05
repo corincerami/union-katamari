b156330ea2dd948cf21df08992f7fef0ceca7ec2
Chris C Cerami <chrisccerami@users.noreply.github.com>
2025-10-05 16:38:39 -0400

    Spawn mobs based on level data

--- main.gd
+++ main.gd
@@ -3,12 +3,21 @@ extends Node
 @export var mob_scene: PackedScene
 var score
 
+var mobs = [
+	{"spawn": [100, 100]},
+	{"spawn": [300, 500]}
+]
+
 func _ready():
 	new_game()
 
 func new_game():
 	score = 0
 	$Player.start($StartPosition.position)
-	$Mob.start($Mob/SpawnPosition.position)
-	$Mob2.start($Mob2/SpawnPosition.position)
-	$Mob3.start($Mob3/SpawnPosition.position)
+	for mob_data in mobs:
+		var mob = mob_scene.instantiate();
+
+		mob.position = Vector2(mob_data.spawn[0], mob_data.spawn[1]);
+
+		# Spawn the mob by adding it to the Main scene.
+		add_child(mob)
