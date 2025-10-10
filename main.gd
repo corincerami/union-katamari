d3fc34a7454f714c5aee332f8bc9887d1c804f41
Corin DaRosa <chrisccerami@users.noreply.github.com>
2025-10-09 23:36:03 -0400

    Make mob personalities random and starting to layout mobs in level 1

--- main.gd
+++ main.gd
@@ -14,21 +14,29 @@ enum Ratings {
 }
 
 var mobs = [
-	{"spawn": [100, 100], "personality": 1},
-	{"spawn": [700, 500], "personality": 1},
-	{"spawn": [800, 200], "personality": 1},
-	{"spawn": [400, 100], "personality": 2},
-	{"spawn": [1000, 500], "personality": 2},
-	{"spawn": [300, 200], "personality": 2},
-	{"spawn": [300, 500], "personality": 2},
-	{"spawn": [100, 500], "personality": 2},
-	{"spawn": [900, 200], "personality": 0},
-	{"spawn": [100, 350], "personality": 0},
-	{"spawn": [300, 350], "personality": 0},
-	{"spawn": [200, 350], "personality": 0},
+{"spawn": [335, 93]},
+{"spawn": [335, 31]},
+{"spawn": [315, 31]},
+{"spawn": [295, 29]},
+{"spawn": [277, 29]},
+{"spawn": [260, 30]},
+{"spawn": [569, 294]},
+{"spawn": [606, 256]},
+{"spawn": [551, 236]},
+{"spawn": [587, 206]},
+{"spawn": [209, 456]},
+{"spawn": [262, 452]},
+{"spawn": [239, 428]},
+{"spawn": [200, 414]},
+{"spawn": [245, 388]},
+{"spawn": [285, 411]},
+{"spawn": [261, 93]},
+{"spawn": [280, 93]},
+{"spawn": [300, 93]},
+{"spawn": [317, 94]}
 ]
 
-var level_time = 15;
+var level_time = 60;
 
 func _ready():
 	level_timer.timeout.connect(_on_level_timer_timeout)
@@ -43,7 +51,6 @@ func new_game():
 		var unique_mob = mob.duplicate();
 		
 		unique_mob.position = Vector2(mob_data.spawn[0], mob_data.spawn[1]);
-		unique_mob.personality = mob_data.personality;
 		# Spawn the mob by adding it to the Main scene.
 		add_child(unique_mob)
 	level_timer.start();
--- mob.gd
+++ mob.gd
@@ -5,8 +5,15 @@ class_name Mob;
 var player: Player;
 var clump_offset: Array;
 @export var clump_index: int;
-@export var speed = 10_000;
-@export var personality: int;
+@export var speed = 7_000;
+var personality: int;
+var rng = RandomNumberGenerator.new()
+
+enum Personalities {
+	Neutral,
+	Positive,
+	Negative
+}
 
 func join_clump(index):
 	if !clump_index:
@@ -17,6 +24,13 @@ func join_clump(index):
 
 func _ready():
 	player = get_parent().get_node("Player");
+	var personality_random = rng.randf_range(0.0, 1.0)
+	if personality_random < 0.15:
+		personality = Personalities.Negative;
+	elif personality_random < 0.3:
+		personality = Personalities.Positive;
+	else:
+		personality = Personalities.Neutral;
 	$CollisionShape2D.disabled = false
 	
 func _physics_process(delta):
@@ -24,9 +38,9 @@ func _physics_process(delta):
 		position = Vector2(player.position.x + clump_offset[0], player.position.y + clump_offset[1]);
 	else:
 		var dist = position.distance_to(player.position);
-		if dist < 100 && personality != 0:
+		if dist < 100 && personality != Personalities.Neutral:
 			velocity = position.direction_to(player.position) * speed * delta;
-			if personality == 2:
+			if personality == Personalities.Negative:
 				velocity *= -1;
 			move_and_slide();
 		
@@ -51,7 +65,7 @@ func _get_clump_offset():
 	if mod == 0:
 		clump_offset = [15, -15];
 	
-#	This will make the first 8 mobs in an inner ring, the second 8 a little furtehr out, and so on
+#	This will make the first 8 mobs in an inner ring, the second 8 a little further out, and so on
 	var ring = floor(clump_index / 8.0) + 1;
 	if ring > 1:
 		clump_offset = [clump_offset[0] * ring, clump_offset[1] * ring];
